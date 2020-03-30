a672d97a83e21b76275fbef2711fbc34317ed61e
OmarSalah26 <23om45ar@gmail.com>
2020-03-30 20:29:01 +0200

    -

--- Program.cs
+++ Program.cs
@@ -10,91 +10,106 @@ namespace os
 {
     class Program
     {
-
+    public   static  double  cpu_usage_For_idel = 0;
         private static DateTime Curtime;
-        static void Main(string[] args)
+        static void ListOfProcesses()
+        {
+            #region CpuUsage
+            // to calc usage for Idel
+          
+
+        // label to return up by go to 
+
+
+            // to get all running processes
+
+            Console.WriteLine("Count : {0}", Process.GetProcesses().Length);
+            foreach (var item in Process.GetProcesses().OrderBy(e => e.ProcessName))
+            {
+                Console.Write(" Process Name   --> " + item.ProcessName + " -  process id : " + item.Id);
+
+                // to get process id 
+                int procID = item.Id;
+
+
+                // to get specific process by id 
+                if (procID != 0)
+                {
+                    try
+                    {
+                        Process p = Process.GetProcessById(procID);
+                        // to give the current time
+                        Curtime = DateTime.Now;
+
+                        // calc cpu usage
+
+                        double cupUsge = (p.TotalProcessorTime.TotalMilliseconds) /
+                                  Curtime.Subtract(p.StartTime).TotalMilliseconds / Convert.ToDouble(Environment.ProcessorCount);
+                        // i will use it to calc the total processor time for all processes
+                        cpu_usage_For_idel += cupUsge;
+                        Console.WriteLine("  --------->  Cpu : {0:0.0000000} %",  cupUsge * 100);
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine("the process not found");
+                    }
+
+
+
+
+                }
+               
+
+
+
+
+            }
+
+            CalcprocessIdel(0, cpu_usage_For_idel);
+
+            #endregion
+
+        }
+
+        static void CalcprocessIdel(int procID ,double cpu_usage_For_idel) {
+            
+
+                // to calc the cpu usage for adle process
+
+                Process pp = Process.GetProcessById(procID);
+
+                //Thread.Sleep(100);
+                Console.WriteLine(" {0}  processId : 0 Cpu Usage : {1:0.0} %", pp.ProcessName, (100 - cpu_usage_For_idel * 100));
+
+
+            
+
+
+        }
+            static void Main(string[] args)
         {
 
 
-            #region taskManager
+        #region taskManager
 
-            #region CpuUsage
-            // to calc usage for Idel
-            double cpu_usage_For_idel = 0;
+        ShowTheListAgain:
 
-                    // label to return up by go to 
-                    ShowTheListAgain:
-
-
-                    // to get all running processes
-
-                Console.WriteLine("Count : {0}", Process.GetProcesses().Length);
-                    foreach (var item in Process.GetProcesses().OrderBy(e => e.ProcessName))
-                    {
-                        Console.Write(" Process Name   --> " + item.ProcessName + " -  process id : " + item.Id);
-
-                        // to get process id 
-                        int procID = item.Id;
-
-
-                        // to get specific process by id 
-                        if (procID != 0)
-                        {
-                            try
-                            {
-                                Process p = Process.GetProcessById(procID);
-                        // to give the current time
-                                Curtime = DateTime.Now;
-
-                                // calc cpu usage
-
-                                double cupUsge = (p.TotalProcessorTime.TotalMilliseconds) /
-                                          Curtime.Subtract(p.StartTime).TotalMilliseconds / Convert.ToDouble(Environment.ProcessorCount);
-                        // i will use it to calc the total processor time for all processes
-                                cpu_usage_For_idel += cupUsge;
-                                Console.WriteLine("  ---------> {0} Cpu : {1:0.0000000} %", p.ProcessName, cupUsge * 100);
-
-                            }
-                            catch (Exception ex)
-                            {
-
-                                Console.WriteLine("the process not found");
-                            }
-
-
-
-
-                        }
-                        else
-                        {
-
-                            // to calc the cpu usage for adle process
-
-                            Process pp = Process.GetProcessById(procID);
-
-                            //Thread.Sleep(100);
-                            Console.WriteLine(" {0} Cpu : {1:0.0} %", pp.ProcessName, (100 - cpu_usage_For_idel * 100));
-
-
-                        }
-
-
-
-
-                    }
-            #endregion
+            ListOfProcesses();
 
 
 
             //#region Kill And Change pirority
 
             Console.WriteLine("\n \n OPtions :");
-            Console.WriteLine("if you need to kill process                    press =>> 1");
-            Console.WriteLine("if you need to change prioirty of process      press =>> 2");
-            Console.WriteLine("if you need to test individual process         press =>> 3");
+            Console.WriteLine("if you need to kill process                            press =>> 1");
+            Console.WriteLine("if you need to change prioirty of process              press =>> 2");
+            Console.WriteLine("if you need to test individual process                 press =>> 3");
+            Console.WriteLine("if you need to Show task manager more than one time    press =>> 4");
 
-            Console.WriteLine("if you need to Show task manager Again         press =>> 4");
-            Console.WriteLine("if you need to Close or Exit From application  press =>> 5");
+            Console.WriteLine("if you need to Show task manager Again                 press =>> 5");
+            Console.WriteLine("if you need to Close or Exit From application          press =>> 6");
 
             //to choose any process to use
             again6:
@@ -285,7 +300,7 @@ namespace os
 
                             double cupUsge = (p.TotalProcessorTime.TotalMilliseconds) /
                                       Curtime.Subtract(p.StartTime).TotalMilliseconds / Convert.ToDouble(Environment.ProcessorCount);
-                            cpu_usage_For_idel += cupUsge;
+                        
                             Console.WriteLine("  ---------> {0} Cpu : {1:0.0000000} %", p.ProcessName, cupUsge * 100);
                                 Console.WriteLine("please press any key to return ");
                                 while (Console.KeyAvailable)
@@ -305,14 +320,23 @@ namespace os
                         }
 
                     goto ShowTheListAgain;
-                // to show the list more than time
+                    // to show the list more than time
+                    case 4:
+                        while (!Console.KeyAvailable)
 
-                case 4:
+                       {
+                            ListOfProcesses();
+
+                            Console.WriteLine("Please press any key to return to task manager");
+                            Thread.Sleep(2000);
+                        }
+                        goto ShowTheListAgain;
+                case 5:
 
                     goto ShowTheListAgain;
                 // To close the console
 
-                case 5:
+                case 6:
                     Environment.Exit(0);
                     break;
 
